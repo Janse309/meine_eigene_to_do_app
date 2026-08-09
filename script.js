efc491a5ab571612aa523d37199c7d9fd8daf2bc
Janse309 <jansimonboecker40@gmail.com>
2026-08-09 16:11:27 +0200

    feat: add loadTasks to fetch data from Firebase

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let allTasks = [];
 
 function init() {
     document.getElementById('form').addEventListener('submit', addTask);
+    loadTasks();
 }
 
 async function addTask(event) {
@@ -41,134 +42,31 @@ async function postData(path = "", data = {}) {
     return responseToJson = await response.json();
 }
 
+async function getData(path = "") {
+    let response = await fetch(BASE_URL + path + ".json");
+    let data = await response.json();
+    console.log(data);
+    return data;
+}
+
+async function loadTasks() {
+    let tasks = await getData("tasks"); // tasks laden
+    allTasks = []; 
+
+    for (let id in tasks) {
+        allTasks.push(tasks[id].Aufgabe);
+    }
+
+    renderTasks();
+}
 
 
 
 
 
+// Delete Data einfügen
+// deleteData Dokumentation lesen
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ============================================================
-// TODO: getData(path) - lädt Tasks von Firebase (GET)
-// ============================================================
-// async function getData(path = "") {
-//     let response = await fetch(BASE_URL + path + ".json");
-//     return await response.json();
-//     // Ergebnis sieht so aus: { "-Nabc123": { Aufgabe: "...", erledigt: false }, "-Nxyz789": {...} }
-//     // (ein Objekt, key = von Firebase generierte ID, value = der Task)
-// }
-
-
-// ============================================================
-// TODO: loadTasks() - füllt allTasks beim Start mit den Firebase-Daten
-// ============================================================
-// async function loadTasks() {
-//     let tasks = await getData("tasks");
-//     allTasks = [];
-//
-//     if (tasks) {
-//         for (let id in tasks) {
-//             allTasks.push({
-//                 id: id,
-//                 Aufgabe: tasks[id].Aufgabe,
-//                 erledigt: tasks[id].erledigt
-//             });
-//         }
-//     }
-//
-//     renderTasks();
-// }
-//
-// WICHTIG: in init() muss loadTasks() aufgerufen werden, sonst passiert das nie
-// function init() {
-//     document.getElementById('form').addEventListener('submit', addTask);
-//     loadTasks();
-// }
-
-
-// ============================================================
-// TODO: addTask() anpassen - jetzt Objekte statt Strings speichern
-// ============================================================
-// async function addTask(event) {
-//     event.preventDefault();
-//
-//     let inputContent = document.getElementById('task-input');
-//     let taskInput = inputContent.value;
-//
-//     let newTask = { Aufgabe: taskInput, erledigt: false };
-//     let response = await postData("tasks", newTask);
-//     // response.name enthält die von Firebase generierte ID des neuen Eintrags
-//
-//     allTasks.push({ id: response.name, Aufgabe: taskInput, erledigt: false });
-//
-//     renderTasks();
-//     inputContent.value = "";
-// }
-
-
-// ============================================================
-// TODO: toggleTask(id, erledigt) - wird beim Anklicken der Checkbox aufgerufen
-// ============================================================
-// async function toggleTask(id, erledigt) {
-//     let task = allTasks.find(t => t.id === id);
-//     task.erledigt = erledigt;
-//
-//     await patchData("tasks/" + id, { erledigt: erledigt });
-//     // PATCH ändert in Firebase NUR das Feld "erledigt" bei GENAU diesem Eintrag
-// }
-
-
-// ============================================================
-// TODO: patchData(path, data) - analog zu postData, aber method: "PATCH"
-// ============================================================
-// async function patchData(path = "", data = {}) {
-//     let response = await fetch(BASE_URL + path + ".json", {
-//         method: "PATCH",
-//         headers: { 'Content-Type': 'application/json', },
-//         body: JSON.stringify(data)
-//     });
-//     return await response.json();
-// }
-
-
-// ============================================================
-// TODO: renderTasks() anpassen
-// ============================================================
-// function renderTasks() {
-//     let taskRef = document.getElementById('content-area');
-//     taskRef.innerHTML = "";
-//     for (let i = 0; i < allTasks.length; i++) {
-//         let task = allTasks[i];
-//         taskRef.innerHTML += `
-//             <ul>
-//                 <li>
-//                     <input type="checkbox" name="task" onchange="toggleTask('${task.id}', this.checked)" ${task.erledigt ? "checked" : ""}>
-//                     <span>${task.Aufgabe}</span>
-//                 </li>
-//             </ul>
-//                 `
-//     }
-// }
 // - allTasks ist jetzt ein Array von Objekten -> task.Aufgabe statt allTasks[i]
 // - onchange ruft toggleTask() mit der id des Tasks und this.checked (neuer Checkbox-Zustand) auf
 // - ${task.erledigt ? "checked" : ""} sorgt dafür, dass bereits erledigte Tasks nach
