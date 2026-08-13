934ba2f8a740163ab9a39245e8f965ac62f4751a
Janse309 <jansimonboecker40@gmail.com>
2026-08-13 17:36:11 +0200

    style: update register section and improve login functionality

--- scripts/login.js
+++ scripts/login.js
@@ -1,24 +1,28 @@
-        function login() {
-            let email = document.getElementById('email');
-            let password = document.getElementById('password');
-            let user = users.find(user => user.email == email.value && user.password == password.value);
-            if (user) {
-                console.log("user gefunden");
-                window.location.href = './todo.html';
-            }
-        }
+function login() {
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let user = users.find(user => user.email == email.value && user.password == password.value);
+    if (user) {
+        console.log("user gefunden");
+        window.location.href = './todo.html';
+    }
+}
 
-        const urlParams = new URLSearchParams(window.location.search);
-        const msg = urlParams.get('msg');
+const urlParams = new URLSearchParams(window.location.search);
+const msg = urlParams.get('msg');
 
-        if (msg) {
-            msgBox.innerHTML = msg;
-            setTimeout(() => {
-                const element = document.getElementById('msgBox');
-                if (element) {
-                    element.remove();
-                }
-            }, 3000);
-        } else {
-            // display:none;
+if (msg) {
+    msgBox.innerHTML = msg;
+    setTimeout(() => {
+        const element = document.getElementById('msgBox');
+        if (element) {
+            element.remove();
         }
+    }, 3000);
+} else {
+    // display:none;
+}
+
+function navigateToRegister() {
+    window.location.href = './register.html';
+}
