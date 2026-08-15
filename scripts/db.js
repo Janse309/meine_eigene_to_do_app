bb02440ad6153831bbd3c6b876450024adbf303a
Janse309 <jansimonboecker40@gmail.com>
2026-08-15 11:54:50 +0200

    test: comment out addUser to test registerUser() instead

--- scripts/db.js
+++ scripts/db.js
@@ -3,11 +3,11 @@ let users = JSON.parse(localStorage.getItem('users')) || [
     { 'email': 'guestuser@web.com', 'password': 'myGuests123' }
 ];
 
-function addUser() {
-    let email = document.getElementById('email');
-    let password = document.getElementById('password');
-    users.push({ email: email.value, password: password.value });
-    window.location.href = './login.html?msg=Du hast dich erfolgreich registriert'
-}
+// function addUser() {
+//     let email = document.getElementById('email');
+//     let password = document.getElementById('password');
+//     users.push({ email: email.value, password: password.value });
+//     window.location.href = './login.html?msg=Du hast dich erfolgreich registriert'
+// }
 
 console.log(users);
