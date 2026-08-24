d7b9123bd43aa19dd05f27d039335025a09d59aa
Janse309 <jansimonboecker40@gmail.com>
2026-08-24 21:26:40 +0200

    refactor: simplify section structure and update task button styling

--- scripts/template.js
+++ scripts/template.js
@@ -1,10 +1,12 @@
 function getTaskTemplate(i) {
     return `
-        <div class="task-card-container">
-            <div draggable="true" class="task-item">
+        <div id="task-${i}" class="task-card-container task" draggable="true" ondragstart="startDragging(${i})" ondragend="this.style.cursor='grab'" onmousedown="this.style.cursor='grabbing'" onmouseup="this.style.cursor='grab'">
+            <div class="task-item">
                 <input class="checkbox" type="checkbox" name="task" onchange="checkOffToDos(this)">
                 <span>${allTasks[i].Aufgabe}</span>
-                <button class="btn to-do-btn" onclick="deleteTask(${i})" class="btn-font"><img src="../assets/icons/delete-icon.svg" alt="delete"></button>
+                <button class="btn to-do-btn" onclick="deleteTask(${i})" class="btn-font">
+                    <img class="trash-icon" src="../assets/icons/delete-icon.svg" alt="delete">
+                </button>
             </div>
         </div>
     `
